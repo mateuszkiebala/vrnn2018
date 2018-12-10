64ec0b7d52d70a39925d4c8db02c9f59f5b84260
Janusz Marcinkiewicz <virrages@gmail.com>
2018-12-10 10:58:20 +0100

    Generalize board to np.array convertion

--- gen/single_figure.py
+++ gen/single_figure.py
@@ -4,9 +4,15 @@ from cairosvg import svg2png
 from scipy import misc
 from IPython.display import SVG
 
-board = chess.Board("8/8/8/8/4N3/8/8/8 w - - 0 1")
-svg = SVG(chess.svg.board(board=board))
-svg2png(svg.data, write_to='tmp.png')
-image = misc.imread('tmp.png')
-os.remove('tmp.png')
 numpy.set_printoptions(threshold=numpy.nan)
+
+def board2array(board):
+    svg = SVG(chess.svg.board(board=board))
+    svg2png(svg.data, write_to='tmp.png')
+    image = misc.imread('tmp.png')
+    os.remove('tmp.png')
+    return image
+
+board = chess.Board("8/8/8/8/4N3/8/8/8 w - - 0 1")
+image = board2array(board)
+print(image)
