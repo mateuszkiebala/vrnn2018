aa90df66395dfbc09abe761e7962bcf7cea51869
Mateusz Kiebala <mateuszkiebala@gmail.com>
2019-01-05 23:22:08 +0100

    parallel pgn gen

--- common/constants.py
+++ common/constants.py
@@ -6,8 +6,10 @@ GOOD_GAMES_IMG_PATH = path.join(file_path, "../games/images/good/")
 BAD_GAMES_IMG_PATH = path.join(file_path, "../games/images/bad/")
 
 GAMES_ARR_PATH = path.join(file_path, "../dataset/")
-BOARDS_FILE_NAME = "boards.npy"
-RESULTS_FILE_NAME = "results.npy"
+BOARDS_FILE_NAME = "book_boards.npy"
+RESULTS_FILE_NAME = "book_results.npy"
+
+BOOK_GAMES_PATH = "games/games.pgn"
 
 DEFAULT_IMAGE_SIZE = 200 # size (width, height) of an image
 
--- gen/pgn_parser.py
+++ gen/pgn_parser.py
@@ -1,4 +1,5 @@
-import os, chess, uuid, argparse, random
+import os, chess, uuid, argparse, random, threading
+import multiprocessing as mp
 import numpy as np
 from chess import svg, pgn
 from cairosvg import svg2png
@@ -8,10 +9,12 @@ from common.constants import *
 
 np.set_printoptions(threshold=np.nan)
 
+
 # Saves board to png with provided name and size
 def board2png(board, name, size=DEFAULT_IMAGE_SIZE, coordinates=False):
     svg = SVG(chess.svg.board(board=board, size=size, coordinates=coordinates))
     svg2png(svg.data, write_to=name)
+
 
 # Returns numpy array from given board
 def board2array(board):
@@ -21,9 +24,15 @@ def board2array(board):
     os.remove(unique_filename)
     return image
 
+
 def board2vector(board):
     array = np.array(board2array(board))
     return array.flatten()
+
+
+def boards_vector_double(prev_board, next_board):
+    return np.array([board2array(prev_board), board2array(next_board)])
+
 
 def boards_vector_triple(prev_board, next_board, diff_board):
     return np.array([board2array(prev_board), board2array(next_board), board2array(diff_board)])
@@ -33,55 +42,58 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--games', type=int, default=10, help='Number of games played')
 args = parser.parse_args()
 
+GOOD_RESULT = [1, 0]
+BAD_RESULT = [0, 1]
 
-class PgnGenerator:
-    GOOD_RESULT = [1, 0]
-    BAD_RESULT = [0, 1]
+class PgnReader:
+    pgn = open(BOOK_GAMES_PATH)
+    lock = threading.Lock()
 
-    def __init__(self, pgn_games_path):
-        self.pgn_games_path = pgn_games_path
-        self.result_boards = []
-        self.result_vector = []
 
-    def generate_data(self):
-        pgn_file = open(self.pgn_games_path)
-        game = pgn.read_game(pgn_file)
-        game_index = 0
-        while game and game_index < args.games:
-            board = game.board()
-            for i, move in enumerate(game.mainline_moves()):
-                boards_vec, result = gen_good(board.copy(), move)
-                self.result_boards.append(boards_vec)
-                self.result_vector.append(result)
+def next_book_game():
+    with PgnReader.lock:
+        return chess.pgn.read_game(PgnReader.pgn)
 
-                boards_vec, result = gen_bad(board.copy(), move)
-                self.result_boards.append(boards_vec)
-                self.result_vector.append(result)
 
-                board.push(move)
-                if i % 10 == 0:
-                    print("Game number: {0}. Move number: {1}".format(game_index, i))
-            game_index += 1
-            game = pgn.read_game(pgn_file)
-        return [self.result_boards, self.result_vector]
+def generate_data(game_num):
+    result_boards = []
+    result_vector = []
+    game = next_book_game()
+    board = game.board()
+    moves_cnt = 0
+    for move in game.mainline_moves():
+        boards_vec, result = gen_good(board.copy(), move)
+        result_boards.append(boards_vec)
+        result_vector.append(result)
+
+        boards_vec, result = gen_bad(board.copy(), move)
+        result_boards.append(boards_vec)
+        result_vector.append(result)
+
+        board.push(move)
+        if moves_cnt % 10 == 0:
+            print("Game: {0}. Move number: {1}".format(game_num, moves_cnt))
+        moves_cnt += 1
+    print("Game: {0} finished with {1} moves".format(game_num, moves_cnt))
+    return [result_boards, result_vector]
 
 
 def gen_good(board, move):
     prev_board = board.copy()
-    board_with_only_start_piece = gen_single_figure_board(move.from_square, prev_board.piece_at(move.from_square))
+    #board_with_only_start_piece = gen_single_figure_board(move.from_square, prev_board.piece_at(move.from_square))
     board.push(move)
-    return [boards_vector_triple(prev_board, board, board_with_only_start_piece), PgnGenerator.GOOD_RESULT]
+    return [boards_vector_double(prev_board, board), GOOD_RESULT]
 
 
 def gen_bad(board, move):
     prev_board = board.copy()
     start_square = move.from_square
     piece_to_move = prev_board.piece_at(start_square)
-    board_with_only_start_piece = gen_single_figure_board(start_square, piece_to_move)
+    #board_with_only_start_piece = gen_single_figure_board(start_square, piece_to_move)
     illegal_move = gen_illegal_move(prev_board, start_square)
     board.set_piece_at(illegal_move.to_square, piece_to_move)
     board.remove_piece_at(illegal_move.from_square)
-    return [boards_vector_triple(prev_board, board, board_with_only_start_piece), PgnGenerator.BAD_RESULT]
+    return [boards_vector_double(prev_board, board), BAD_RESULT]
 
 
 def gen_single_figure_board(square, piece):
@@ -97,14 +109,6 @@ def gen_illegal_move(board, square):
     random.shuffle(illegal_moves)
     return chess.Move.from_uci(illegal_moves[0])
 
-PIECE_MOVES = {
-    1: [1, 0, 0, 0, 1],
-    2: [0, 0, 0, 1, 4],
-    3: [0, 0, 1, 0, 8],
-    4: [1, 1, 0, 0, 8],
-    5: [1, 1, 1, 0, 8],
-    6: [1, 1, 1, 0, 1]
-}
 
 PIECE_TYPE = {
     1: [1, 0, 0, 0, 0, 0],
@@ -119,15 +123,20 @@ PIECE_TYPE = {
 def generate_piece_type_result(board, move):
     piece = board.piece_at(move.from_square)
     return PIECE_TYPE[piece.piece_type]
-    #return [piece.piece_type, int(piece.color)] + PIECE_MOVES[piece.piece_type]
+
 
 if __name__ == '__main__':
-    gen = PgnGenerator("games/games.pgn")
+    pool = mp.Pool(processes=mp.cpu_count())
+    all_boards = []
+    all_results = []
+    for result in pool.map(generate_data, range(1, args.games + 1)):
+        all_boards.extend(result[0])
+        all_results.extend(result[1])
+
     boards_path, results_path = dataset_path()
 
     os.makedirs(os.path.dirname(boards_path), exist_ok=True)
     os.makedirs(os.path.dirname(results_path), exist_ok=True)
 
-    [boards, results] = gen.generate_data()
-    np.save(boards_path, np.array(boards))
-    np.save(results_path, np.array(results))
+    np.save(boards_path, np.array(all_boards))
+    np.save(results_path, np.array(all_results))
