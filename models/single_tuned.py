568266a660ddbee9a0d0afda52a33ce898b736b2
Tomasz Knopik <tomasz.knopik@gmail.com>
2019-01-13 12:49:58 +0100

    Add dense and dropout

--- models/single_tuned.py
+++ models/single_tuned.py
@@ -40,17 +40,16 @@ def compiled_single_model(model_input_shape):
 
     model = Conv2D(128, (3, 3), activation='relu')(model)
     model = BatchNormalization(axis=3)(model)
-    
     model = Activation('relu')(model)
     model = MaxPooling2D((3, 3))(model)
 
     model = Conv2D(256, (3, 3), activation='relu')(model)
     model = MaxPooling2D((3, 3))(model)
 
-    model = Conv2D(512, (3, 3), activation='relu')(model)
-    model = MaxPooling2D((3, 3))(model)
-
     model = Flatten()(model)
+
+    model = Dense(256, activation='relu')(model)
+    model = Dropout(.15)(model)
 
     model = Dense(128, activation='relu')(model)
     model = Dropout(.25)(model)
